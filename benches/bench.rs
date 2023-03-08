bb9d436f71cd329b3e6c7c126a1a732d8ba372a1
Kyle Matsuda <kyle.yoshio.matsuda@gmail.com>
2023-03-08 10:40:04 -0700

    separate chaining with vec

---
+++ benches/bench.rs
@@ -0,0 +1,224 @@
+use cornedbeef::CbHashMap;
+use criterion::{black_box, criterion_group, criterion_main, BenchmarkId, Criterion};
+use std::collections::HashMap as StdHashMap;
+
+static SIZE: usize = 100_000;
+
+// A random key iterator.
+// Copied from rust-lang/hashbrown
+#[derive(Clone, Copy)]
+struct RandomKeys {
+    state: usize,
+}
+
+impl RandomKeys {
+    fn new() -> Self {
+        RandomKeys { state: 0 }
+    }
+}
+
+impl Iterator for RandomKeys {
+    type Item = usize;
+    fn next(&mut self) -> Option<usize> {
+        // Add 1 then multiply by some 32 bit prime.
+        self.state = self.state.wrapping_add(1).wrapping_mul(3_787_392_781);
+        Some(self.state)
+    }
+}
+
+macro_rules! bench_new {
+    ($map:ident, $size:expr) => {
+        |b| {
+            b.iter(|| {
+                let _ = black_box($map::<usize, usize>::with_capacity($size));
+            })
+        }
+    };
+}
+
+pub fn new(c: &mut Criterion) {
+    let mut group = c.benchmark_group("new");
+    for size in [0, 10000] {
+        group.bench_function(BenchmarkId::new("std", size), bench_new!(StdHashMap, size));
+        group.bench_function(BenchmarkId::new("cb", size), bench_new!(CbHashMap, size));
+    }
+    group.finish();
+}
+
+macro_rules! bench_grow {
+    ($map:ident, $it:expr, $len:expr) => {
+        |b| {
+            let mut map = $map::new();
+            b.iter(|| {
+                for i in $it {
+                    black_box(map.insert(i, [i; $len]));
+                }
+            })
+        }
+    };
+}
+
+pub fn insert_grow_seq(c: &mut Criterion) {
+    let mut group = c.benchmark_group("insert_grow_seq");
+
+    {
+        const LEN: usize = 4;
+        group.bench_function(
+            BenchmarkId::new("std", LEN),
+            bench_grow!(StdHashMap, 0..SIZE, LEN),
+        );
+        group.bench_function(
+            BenchmarkId::new("cb", LEN),
+            bench_grow!(CbHashMap, 0..SIZE, LEN),
+        );
+    }
+
+    {
+        const LEN: usize = 100;
+        group.bench_function(
+            BenchmarkId::new("std", LEN),
+            bench_grow!(StdHashMap, 0..SIZE, LEN),
+        );
+        group.bench_function(
+            BenchmarkId::new("cb", LEN),
+            bench_grow!(CbHashMap, 0..SIZE, LEN),
+        );
+    }
+    group.finish();
+}
+
+pub fn insert_grow_random(c: &mut Criterion) {
+    let mut group = c.benchmark_group("insert_grow_random");
+    let seq = RandomKeys::new();
+
+    {
+        const LEN: usize = 4;
+        group.bench_function(
+            BenchmarkId::new("std", LEN),
+            bench_grow!(StdHashMap, seq.take(SIZE), LEN),
+        );
+        group.bench_function(
+            BenchmarkId::new("cb", LEN),
+            bench_grow!(CbHashMap, seq.take(SIZE), LEN),
+        );
+    }
+
+    {
+        const LEN: usize = 100;
+        group.bench_function(
+            BenchmarkId::new("std", LEN),
+            bench_grow!(StdHashMap, seq.take(SIZE), LEN),
+        );
+        group.bench_function(
+            BenchmarkId::new("cb", LEN),
+            bench_grow!(CbHashMap, seq.take(SIZE), LEN),
+        );
+    }
+    group.finish();
+}
+
+macro_rules! bench_lookup {
+    ($map:ident, $size:expr, $len:expr) => {
+        |b| {
+            let mut map = $map::new();
+            let seq = RandomKeys::new();
+
+            for i in seq.take($size) {
+                map.insert(i, [i; $len]);
+            }
+            b.iter(|| {
+                for i in seq.take($size) {
+                    black_box(map.get(&i));
+                }
+            })
+        }
+    };
+}
+
+pub fn lookup(c: &mut Criterion) {
+    let mut group = c.benchmark_group("lookup");
+
+    {
+        const LEN: usize = 4;
+        group.bench_function(
+            BenchmarkId::new("std", LEN),
+            bench_lookup!(StdHashMap, SIZE, LEN),
+        );
+        group.bench_function(
+            BenchmarkId::new("cb", LEN),
+            bench_lookup!(CbHashMap, SIZE, LEN),
+        );
+    }
+
+    {
+        const LEN: usize = 100;
+        group.bench_function(
+            BenchmarkId::new("std", LEN),
+            bench_lookup!(StdHashMap, SIZE, LEN),
+        );
+        group.bench_function(
+            BenchmarkId::new("cb", LEN),
+            bench_lookup!(CbHashMap, SIZE, LEN),
+        );
+    }
+
+    group.finish();
+}
+
+macro_rules! bench_lookup_miss {
+    ($map:ident, $size:expr, $len:expr) => {
+        |b| {
+            let mut map = $map::new();
+            let mut seq = RandomKeys::new();
+
+            for i in (&mut seq).take($size) {
+                map.insert(i, [i; $len]);
+            }
+            b.iter(|| {
+                for i in (&mut seq).take($size) {
+                    black_box(map.get(&i));
+                }
+            })
+        }
+    };
+}
+
+pub fn lookup_miss(c: &mut Criterion) {
+    let mut group = c.benchmark_group("lookup_miss");
+
+    {
+        const LEN: usize = 4;
+        group.bench_function(
+            BenchmarkId::new("std", LEN),
+            bench_lookup_miss!(StdHashMap, SIZE, LEN),
+        );
+        group.bench_function(
+            BenchmarkId::new("cb", LEN),
+            bench_lookup_miss!(CbHashMap, SIZE, LEN),
+        );
+    }
+
+    {
+        const LEN: usize = 100;
+        group.bench_function(
+            BenchmarkId::new("std", LEN),
+            bench_lookup_miss!(StdHashMap, SIZE, LEN),
+        );
+        group.bench_function(
+            BenchmarkId::new("cb", LEN),
+            bench_lookup_miss!(CbHashMap, SIZE, LEN),
+        );
+    }
+
+    group.finish();
+}
+
+criterion_group!(
+    benches,
+    new,
+    insert_grow_seq,
+    insert_grow_random,
+    lookup,
+    lookup_miss
+);
+criterion_main!(benches);
---
+++ src/lib.rs
@@ -0,0 +1,157 @@
+use core::hash::{BuildHasher, Hasher};
+use std::collections::hash_map::DefaultHasher;
+
+// Use std's default hasher.
+pub type DefaultHashBuilder = core::hash::BuildHasherDefault<DefaultHasher>;
+
+fn make_hash<S: BuildHasher, K: core::hash::Hash>(build_hasher: &S, key: &K) -> u64 {
+    let mut hasher = build_hasher.build_hasher();
+    key.hash(&mut hasher);
+    hasher.finish()
+}
+
+/// Naive first Map using separate chaining for now.
+pub struct CbHashMap<K, V, S: BuildHasher = DefaultHashBuilder> {
+    hasher: S,
+    n_buckets: usize,
+    n_items: usize,
+    storage: Box<[Vec<(K, V)>]>,
+}
+
+impl<K, V> CbHashMap<K, V> {
+    pub fn new() -> Self {
+        Self::with_capacity(0)
+    }
+
+    pub fn with_capacity(capacity: usize) -> Self {
+        let capacity = if capacity == 0 {
+            capacity
+        } else if capacity < 16 {
+            16
+        } else {
+            1 << (capacity.ilog2() + 1)
+        };
+        let storage = (0..capacity)
+            .map(|_| Vec::new())
+            .collect::<Vec<_>>()
+            .into_boxed_slice();
+        Self {
+            hasher: DefaultHashBuilder::default(),
+            n_buckets: capacity,
+            n_items: 0,
+            storage,
+        }
+    }
+}
+
+impl<K, V> Default for CbHashMap<K, V> {
+    fn default() -> Self {
+        Self::new()
+    }
+}
+
+impl<K, V> CbHashMap<K, V>
+where
+    K: PartialEq + Eq + core::hash::Hash,
+{
+    pub fn insert(&mut self, k: K, v: V) -> Option<V> {
+        if self.needs_grow() {
+            self.grow_table();
+        }
+        let index = self.key_to_index(&k);
+        match self.get_mut_inner(&k, index) {
+            Some(vv) => {
+                let mut v = v;
+                std::mem::swap(&mut v, vv);
+                Some(v)
+            }
+            None => {
+                self.insert_at(index, k, v);
+                None
+            }
+        }
+    }
+
+    pub fn get(&self, k: &K) -> Option<&V> {
+        let index = self.key_to_index(k);
+        self.get_inner(k, index)
+    }
+
+    fn get_inner(&self, k: &K, index: usize) -> Option<&V> {
+        self.storage[index]
+            .iter()
+            .find(|(kk, _)| k == kk)
+            .map(|(_, v)| v)
+    }
+
+    pub fn get_mut(&mut self, k: &K) -> Option<&mut V> {
+        let index = self.key_to_index(k);
+        self.get_mut_inner(k, index)
+    }
+
+    fn get_mut_inner(&mut self, k: &K, index: usize) -> Option<&mut V> {
+        self.storage[index]
+            .iter_mut()
+            .find(|(kk, _)| k == kk)
+            .map(|(_, v)| v)
+    }
+
+    pub fn len(&self) -> usize {
+        self.n_items
+    }
+
+    fn insert_at(&mut self, index: usize, k: K, v: V) {
+        self.storage[index].push((k, v));
+        self.n_items += 1;
+    }
+
+    fn key_to_index(&self, k: &K) -> usize {
+        usize::rem_euclid(make_hash(&self.hasher, &k) as usize, self.n_buckets)
+    }
+
+    fn needs_grow(&self) -> bool {
+        // Load factor set at 7/8 for now
+        self.n_buckets == 0 || ((self.n_items * 8).saturating_div(self.n_buckets) > 7)
+    }
+
+    fn grow_table(&mut self) {
+        let capacity = if self.n_buckets == 0 {
+            16
+        } else {
+            self.n_buckets * 2
+        };
+        let new_storage = (0..capacity)
+            .map(|_| Vec::new())
+            .collect::<Vec<_>>()
+            .into_boxed_slice();
+        let old_storage = std::mem::replace(&mut self.storage, new_storage);
+        self.n_buckets = capacity;
+
+        for chain in Vec::from(old_storage).into_iter() {
+            for (k, v) in chain.into_iter() {
+                let index = self.key_to_index(&k);
+                self.storage[index].push((k, v));
+            }
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::CbHashMap;
+
+    #[test]
+    fn insert() {
+        let mut map = CbHashMap::new();
+
+        for i in 0..1000 {
+            map.insert(i, i);
+        }
+
+        assert_eq!(map.len(), 1000);
+
+        for i in 0..1000 {
+            assert!(map.get(&i).is_some());
+        }
+    }
+}
